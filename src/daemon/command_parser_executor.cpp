365e9d6eafd941e1d62373c86580938816dad512
Tyler Baker <tyler@foundries.io>
2019-01-24 13:44:55 -0800

    daemon: haven branding
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- src/daemon/command_parser_executor.cpp
+++ src/daemon/command_parser_executor.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2019, Haven Protocol
 // Copyright (c) 2014-2018, The Monero Project
 // 
 // All rights reserved.
@@ -676,7 +677,7 @@ bool t_command_parser_executor::sync_info(const std::vector<std::string>& args)
 
 bool t_command_parser_executor::version(const std::vector<std::string>& args)
 {
-  std::cout << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << std::endl;
+  std::cout << "Haven '" << HAVEN_RELEASE_NAME << "' (v" << HAVEN_VERSION_FULL << ")" << std::endl;
   return true;
 }
 
--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2019, Haven Protocol
 // Copyright (c) 2014-2018, The Monero Project
 //
 // All rights reserved.
@@ -119,7 +120,7 @@ int main(int argc, char const * argv[])
 
     if (command_line::get_arg(vm, command_line::arg_help))
     {
-      std::cout << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
+      std::cout << "Haven '" << HAVEN_RELEASE_NAME << "' (v" << HAVEN_VERSION_FULL << ")" << ENDL << ENDL;
       std::cout << "Usage: " + std::string{argv[0]} + " [options|settings] [daemon_command...]" << std::endl << std::endl;
       std::cout << visible_options << std::endl;
       return 0;
@@ -128,7 +129,7 @@ int main(int argc, char const * argv[])
     // Monero Version
     if (command_line::get_arg(vm, command_line::arg_version))
     {
-      std::cout << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL;
+      std::cout << "Haven '" << HAVEN_RELEASE_NAME << "' (v" << HAVEN_VERSION_FULL << ")" << ENDL;
       return 0;
     }
 
@@ -284,7 +285,7 @@ int main(int argc, char const * argv[])
       tools::set_max_concurrency(command_line::get_arg(vm, daemon_args::arg_max_concurrency));
 
     // logging is now set up
-    MGINFO("Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")");
+    MGINFO("Haven '" << HAVEN_RELEASE_NAME << "' (v" << HAVEN_VERSION_FULL << ")");
 
     MINFO("Moving from main() into the daemonize now.");
 
